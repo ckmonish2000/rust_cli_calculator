62838e1f2c589f2acede99e39125ff57e8322cf2
monish karunakaran <ckmonish2000@gmail.com>
2022-02-06 16:02:13 +0530

    simple add function rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::env;
+
+
+
 
 fn main() {
-  
+//   let Args: Vec<String> = env::args().skip(1).collect();
+  let op_1 = env::args().nth(1).expect("need 2 num");
+  let symbol = env::args().nth(2).expect("mention the operation");
+  let op_2 = env::args().nth(3).expect("need 2 num");
+
+  let op_one:f32 = op_1.parse().expect("cound not parse num 1");
+  let op_two:f32 = op_2.parse().expect("cound not parse num 2");
+
+  let val = match symbol.as_str() {
+    "+"=> op_one+op_two,
+      _ => 0.0
+  };
+
+  println!("{} {} {} = {}",op_one,symbol,op_two,val);
+
 }
