2ccbd14d76d810e98ae2db530c98d8ab4747ec85
monish karunakaran <ckmonish2000@gmail.com>
2022-02-06 16:13:20 +0530

    check if flag

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@ use std::env;
 
 fn main() {
 
-  let op_1 = env::args().nth(1).expect("need 2 num");
-  let symbol = env::args().nth(2).expect("mention the operation");
-  let op_2 = env::args().nth(3).expect("need 2 num");
+  let flag = env::args().nth(1).expect("please pass a flag");
 
+  
+
+  let check_flag =flag.chars().next().unwrap();
+  let flag_start = "-".chars().next().unwrap();
+  assert_eq!(check_flag,flag_start);
+
+
+
+
+  let op_1 = env::args().nth(2).expect("need 2 num");
+  let symbol = env::args().nth(3).expect("mention the operation");
+  let op_2 = env::args().nth(4).expect("need 2 num");
+
+  
   let op_one:f32 = op_1.parse().expect("cound not parse num 1");
   let op_two:f32 = op_2.parse().expect("cound not parse num 2");
 
